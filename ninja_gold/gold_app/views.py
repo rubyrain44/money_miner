26c5bd2faae6c6bb37828a572f37cbe8349b22fc
rubyrain44 <rubyrain44@yahoo.com>
2024-03-23 11:32:38 -0400

    updates to css and began form logic

--- ninja_gold/gold_app/views.py
+++ ninja_gold/gold_app/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    if 'gold' in request.session:
+        gold = request.session['gold']
+        context = {
+            'gold' : gold
+        }
+    else:
+        request.session['gold'] = 0
+        context = {
+            'gold' : request.session['gold']
+        }
+    return render(request, "index.html", context)
+
+def process(request):
+    gold = request.session['gold']
+    if request.POST['process_money_form'] == 'farm':
+        pass
+    elif request.POST['process_money_form'] == 'cave':
+        pass
+    elif request.POST['process_money_form'] == 'house':
+        pass
+    else:
+        pass
+    return redirect('/')
