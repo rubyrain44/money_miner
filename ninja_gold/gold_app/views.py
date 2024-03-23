55ef5a4e4ad05035832a5793f63cba2bf0c8b196
rubyrain44 <rubyrain44@yahoo.com>
2024-03-23 11:43:13 -0400

    fully functional routes

--- ninja_gold/gold_app/views.py
+++ ninja_gold/gold_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+import random
 
 # Create your views here.
 def index(request):
@@ -17,11 +18,34 @@ def index(request):
 def process(request):
     gold = request.session['gold']
     if request.POST['process_money_form'] == 'farm':
-        pass
+        found_money = random.randint(10, 20)
+        gold += found_money
+        request.session['gold'] = gold
+        context = {
+                'gold' : gold
+            }
+        return render(request, "index.html", context)
     elif request.POST['process_money_form'] == 'cave':
-        pass
+        found_money = random.randint(5, 10)
+        gold += found_money
+        request.session['gold'] = gold
+        context = {
+                'gold' : gold
+            }
+        return render(request, "index.html", context)
     elif request.POST['process_money_form'] == 'house':
-        pass
+        found_money = random.randint(2, 5)
+        gold += found_money
+        request.session['gold'] = gold
+        context = {
+                'gold' : gold
+            }
+        return render(request, "index.html", context)
     else:
-        pass
-    return redirect('/')
+        found_money = random.randint(-50, 50)
+        gold += found_money
+        request.session['gold'] = gold
+        context = {
+                'gold' : gold
+            }
+        return render(request, "index.html", context)
